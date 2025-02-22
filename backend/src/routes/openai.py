4d9d3cbc636f5d12d1f52d5d4f5e22fbd948f9d3
brightlightkim <taeyangk95@naver.com>
2025-02-22 06:46:57 -0500

    fix the prompt for the llm call

--- backend/src/routes/openai.py
+++ backend/src/routes/openai.py
@@ -3,18 +3,29 @@ from typing import Annotated
 from pydantic import BaseModel
 
 import sys
+
 sys.path.append("..")
 from controllers.openai_chat_controller import openai_chat_controller
 
-class Prompt(BaseModel):
-  content: str
 
-openai_router = APIRouter(prefix="/openai")
+class PromptRequest(BaseModel):
+    prompt: str
+    character: str
+
+
+openai_router = APIRouter(prefix="/chat")
+
 
 # style:
 # - ted: default
 # - family: family member
 # - celeb: celebrity
-@openai_router.post("/chat/{sessionId}/{style}")
-def openai_chat(sessionId: str, style: str, prompt: Annotated[Prompt, Query()]):
-  return openai_chat_controller(sessionId, style, prompt.content)
+@openai_router.post("/")
+def openai_chat(request: PromptRequest):
+    response = openai_chat_controller(
+        character=request.character, prompt=request.prompt
+    )
+    return {
+        "status": 200,
+        "text": response,
+    }
